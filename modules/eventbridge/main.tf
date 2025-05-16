bf0905e72e0158ea2ad1df6246a4d0817d15380f
Harish <107391575+8555936327@users.noreply.github.com>
2025-05-16 14:41:19 +0530

    Update main.tf

--- modules/eventbridge/main.tf
+++ modules/eventbridge/main.tf
@@ -3,13 +3,23 @@ resource "aws_cloudwatch_event_rule" "parameter_change" {
   description = "Triggers when Parameter Store is updated"
 
   event_pattern = jsonencode({
-    source = ["aws.ssm"]
-    detail-type = ["Parameter Store Change"]
+    source       = ["aws.ssm"]
+    detail-type  = ["Parameter Store Change"]
   })
+}
+
+# EventBridge Target: Send SNS Notification on Parameter Change
+resource "aws_cloudwatch_event_target" "sns_target" {
+  rule      = aws_cloudwatch_event_rule.parameter_change.name
+  target_id = "SendSNSNotification"
+  arn       = var.sns_topic_arn  # Referencing SNS ARN from the SNS module
 }
 
 output "event_rule_arn" {
   description = "ARN of the EventBridge rule"
   value       = aws_cloudwatch_event_rule.parameter_change.arn
 }
-
+variable "sns_topic_arn" {
+  description = "ARN of the SNS topic from the SNS module"
+  type        = string
+}
