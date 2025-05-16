42537644043442b59c9d7883a6e16de934940fc8
Harish <107391575+8555936327@users.noreply.github.com>
2025-05-16 14:49:59 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "user_email" {
+  description = "Email address for receiving Parameter Store update notifications"
+  type        = string
+}
+
+variable "sns_topic_arn" {
+  description = "ARN of the SNS topic from SNS module"
+  type        = string
+}
+
+variable "event_rule_arn" {
+  description = "ARN of the EventBridge rule"
+  type        = string
+}
+
+variable "parameter_arn" {
+  description = "ARN of the Parameter Store configuration"
+  type        = string
+}
+
+variable "lambda_arn" {
+  description = "ARN of the Lambda function"
+  type        = string
+}
