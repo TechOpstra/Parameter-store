8423f83cea5bb5f8f699f04acd7c630a97386175
Harish <107391575+8555936327@users.noreply.github.com>
2025-05-16 14:53:46 +0530

    Update funcion.py

--- funcion.py
+++ funcion.py
@@ -1,24 +1,17 @@
 import json
 import boto3
-import os
 
+# Initialize AWS SSM client
 ssm = boto3.client('ssm')
-sns = boto3.client('sns')
 
 def lambda_handler(event, context):
     parameter_name = "/app/config/api_key"
+    
+    # Fetch the Parameter Store value
     response = ssm.get_parameter(Name=parameter_name, WithDecryption=True)
     api_key = response["Parameter"]["Value"]
 
-    message = f"Updated API Key from Parameter Store: {api_key}"
-
-    sns.publish(
-        TopicArn=os.environ["SNS_TOPIC_ARN"],
-        Message=message,
-        Subject="Parameter Store Update Notification"
-    )
-
     return {
         "statusCode": 200,
-        "body": json.dumps({"message": "Notification Sent!"})
+        "body": json.dumps({"message": f"Fetched API Key: {api_key}"})
     }
