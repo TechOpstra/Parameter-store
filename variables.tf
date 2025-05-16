722f930c1066064683a73a89b7aac150f711b36d
Harish <107391575+8555936327@users.noreply.github.com>
2025-05-16 14:56:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
+variable "region" {
+  description = "aws region"
+  type        = string
+  default     = "us-east-1"
+}
+
+
+
 variable "user_email" {
   description = "Email address for receiving Parameter Store update notifications"
   type        = string
