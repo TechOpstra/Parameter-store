5a79cbc4be49d578ecef933c13a833976c7048f8
Harish <107391575+8555936327@users.noreply.github.com>
2025-05-16 15:00:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,29 +4,8 @@ variable "region" {
   default     = "us-east-1"
 }
 
-
-
 variable "user_email" {
   description = "Email address for receiving Parameter Store update notifications"
   type        = string
-}
-
-variable "sns_topic_arn" {
-  description = "ARN of the SNS topic from SNS module"
-  type        = string
-}
-
-variable "event_rule_arn" {
-  description = "ARN of the EventBridge rule"
-  type        = string
-}
-
-variable "parameter_arn" {
-  description = "ARN of the Parameter Store configuration"
-  type        = string
-}
-
-variable "lambda_arn" {
-  description = "ARN of the Lambda function"
-  type        = string
+  default     = "sbvh1437@gmail.com"
 }
